29db82231c83d6acdf0982aba338eb63df6e7fb3
Simone Viglione <zimoviglio51@gmail.com>
2023-05-22 18:38:04 +0200

    correzione grafica

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,9 +43,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
+                        <li class="nav-item d-flex">
                             <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
-                            <a href="{{ route('admin.projects.index') }}"> {{ __('Progetti') }}</a>
+                            <a class="nav-link" href="{{ route('admin.projects.index') }}"> {{ __('Progetti') }}</a>
                         </li>
                     </ul>
 
